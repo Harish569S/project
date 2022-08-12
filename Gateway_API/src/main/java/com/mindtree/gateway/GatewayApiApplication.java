645f98f553c32be2800448599e60de24e90d024e
mindc1jun109 <mindc1jun109@c1jun48.iiht.tech>
2022-08-12 09:28:01 +0530

    new app

--- Gateway_API/src/main/java/com/mindtree/gateway/GatewayApiApplication.java
+++ Gateway_API/src/main/java/com/mindtree/gateway/GatewayApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApiApplication {
 
 	public static void main(String[] args) {
+		//ok this is to update
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
